3fe917dcc8b99acb08e80eb354cbb4f1e5ad74fd
basilioarth <basilio.arth@gmail.com>
2025-11-29 17:06:32 -0300

    Implementando Express e Criando o Register

---
+++ src/models/user.model.ts
@@ -0,0 +1,22 @@
+import { Field, ObjectType, ID, GraphQLISODateTime } from "type-graphql";
+
+@ObjectType()
+export class UserModel {
+    @Field(() => ID)
+    id!: string
+
+    @Field(() => String)
+    name!: string
+
+    @Field(() => String)
+    email!: string
+
+    @Field(() => String)
+    password!: string
+
+    @Field(() => GraphQLISODateTime)
+    createdAt!: Date
+
+    @Field(() => GraphQLISODateTime)
+    updatedAt!: Date
+}
---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import jwt, { Secret, SignOptions } from "jsonwebtoken"
+
+export type JwtPayload = {
+    id: string
+    email: string
+}
+
+export const signJwt = (payload: JwtPayload, expiresIn?: string) => {
+    const secret: Secret = process.env.JWT_SECRET as unknown as Secret;
+    const expiration = expiresIn;
+
+    let options: SignOptions;
+
+    if (expiration) {
+        options = {
+            expiresIn: expiration as unknown as NonNullable<SignOptions['expiresIn']>
+        }
+    }
+
+    return jwt.sign(payload, secret, options);
+}
